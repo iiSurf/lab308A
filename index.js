0904ae7fbfe3dace927fd329bb2920d16e91e079
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-07-17 23:41:08 -0700

    still attempting part two. But I am still stuck.

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function initialLoad() {
       const option = document.createElement("option");
       // Setting the value of the option to the breed's id
       option.value = breed.id;
-      // Setting the display text of the option to the breed's name
+      // Setting the breed's name to dispaly text option
       option.textContent = breed.name;
       // Append the option to the breedSelect dropdown
       breedSelect.appendChild(option);
@@ -79,7 +79,22 @@ initialLoad();
    * - Add a call to this function to the end of your initialLoad function above to create the initial carousel.
    */
 
-  breedSelect.addEventListener()
+  async function breedSelectInfo(breedId) {
+    try {
+      const apiUrl = ''; // TODO: add url
+      const response = await fetch(apiUrl, {
+        headers: {
+          "x-api-key": API_KEY,
+        },
+      });
+      
+    } catch (err) {
+      console.error(err);
+    }
+    
+  }
+
+  // breedSelect.addEventListener()
 
   /**
    * 3. Fork your own sandbox, creating a new one named "JavaScript Axios Lab."
@@ -93,6 +108,48 @@ initialLoad();
    *   by setting a default header with your API key so that you do not have to
    *   send it manually with all of your requests! You can also set a default base URL!
    */
+
+  // async function initialLoad() {
+  //   try {
+  //     // URL for retrieving cat breeds
+  //     const apiUrl = "https://api.thecatapi.com/v1/breeds";
+  
+  //     // Fetch the data from the Cat API
+  //     const response = await axios.get(apiUrl, {
+  //       headers: {
+  //         // Including my API key in the request headers
+  //         "x-api-key": API_KEY,
+  //       },
+  //     });
+  
+  //     // Checking if the response is not good
+  //     if (!response.ok) {
+  //       // Throw an error if the fetch request failed
+  //       throw new Error("Can not find cat breeds");
+  //     }
+  
+  //     // Parse the JSON response to get list of cat breeds
+  //     const breeds = await response.json();
+  
+  //     // Looping through each breed
+  //     breeds.forEach((breed) => {
+  //       // Creating a new <option> element for each breed
+  //       const option = document.createElement("option");
+  //       // Setting the value of the option to the breed's id
+  //       option.value = breed.id;
+  //       // Setting the breed's name to dispaly text option
+  //       option.textContent = breed.name;
+  //       // Append the option to the breedSelect dropdown
+  //       breedSelect.appendChild(option);
+  //     });
+  
+  //     // ERRAWR handling
+  //   } catch (err) {
+  //     console.error(err, 'This is an ERRAWR!!! I am Reptar, here me Rawr! ');
+  //   }
+  // }
+  // initialLoad();
+
 
   export async function favourite(imgId) {
     // your code here
